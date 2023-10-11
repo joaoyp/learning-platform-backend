b2139cddb49d6261a050b23857a04e011ebb80a7
joaoyp <joaoyp.dev@gmail.com>
2023-10-11 17:18:09 +0100

    First Commit

---
+++ src/main/java/com/joaoyp/learningplatformbackend/LearningPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.joaoyp.learningplatformbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningPlatformBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningPlatformBackendApplication.class, args);
+    }
+
+}
