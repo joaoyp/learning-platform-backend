3bc4367aea157f28c56335ad28747ae1a5b1f3e4
joaoyp <joaoyp.dev@gmail.com>
2023-10-12 23:00:21 +0100

    Login's response with message and token

--- src/main/java/com/joaoyp/learningplatformbackend/controllers/users/auth/AuthController.java
+++ src/main/java/com/joaoyp/learningplatformbackend/controllers/users/auth/AuthController.java
@@ -1,19 +1,16 @@
 package com.joaoyp.learningplatformbackend.controllers.users.auth;
 
-import com.fasterxml.jackson.core.JsonPointer;
-import com.joaoyp.learningplatformbackend.dtos.LoginResponseDTO;
 import com.joaoyp.learningplatformbackend.dtos.UserDTO;
 import com.joaoyp.learningplatformbackend.models.UserModel;
 import com.joaoyp.learningplatformbackend.services.TokenService;
 import com.joaoyp.learningplatformbackend.services.UserService;
 import jakarta.validation.Valid;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -50,10 +47,18 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDTO> loginUser(@RequestBody UserDTO userDTO){
-        var usernamePassword = new UsernamePasswordAuthenticationToken(userDTO.username(), userDTO.password());
-        var auth = authenticationManager.authenticate(usernamePassword);
-        var token = tokenService.gerarToken((UserModel) auth.getPrincipal());
-        return ResponseEntity.ok(new LoginResponseDTO(token));
+    public ResponseEntity<Map<String, String>> loginUser(@RequestBody UserDTO userDTO){
+        Map<String, String> response = new HashMap<>();
+        try {
+            var usernamePassword = new UsernamePasswordAuthenticationToken(userDTO.username(), userDTO.password());
+            var auth = authenticationManager.authenticate(usernamePassword);
+            var token = tokenService.gerarToken((UserModel) auth.getPrincipal());
+            response.put("message", "Login Successfully");
+            response.put("token", token);
+            return ResponseEntity.ok(response);
+        }catch (AuthenticationException e){
+            response.put("message", "Invalid Username or Password");
+            return ResponseEntity.badRequest().body(response);
+        }
     }
 }
